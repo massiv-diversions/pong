dc648fa92f2aba305957dc50d9d6e6f01ac6b8fe
Johnny Wolfe <piogre314@gmail.com>
2013-06-16 18:42:27 -0700

    first commit
    why can't I add libraries?

---
+++ src/PongBall.java
@@ -0,0 +1,36 @@
+//draft of pong ball entity, proof of concept
+
+//created piogre 6/16
+
+//draft version notes:
+//constant x velocity (10) back and forth, xPos range 0 to 100
+//y velocity changes, yPos ranges 0 to 50
+
+Public class PongBall implements PhysObj{
+	
+	private int posX;
+	private int posY;
+	private float velY;
+	private int velX;
+	
+	public PongBall(){
+		
+		posX = 50;
+		posY = 25;
+		
+		Random gen = new Random();
+		
+		if(gen.nextBoolean ){
+			
+			
+			
+		}else{
+			
+			
+			
+		}//end if
+		
+		
+	}//end random velX and velY constructor
+	
+}//end PongBall
