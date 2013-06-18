0158aabbcc3cd78a6c4a1199f73a4239b1da7229
Cooper Johnson <lavosprime@gmail.com>
2013-06-17 22:15:16 -0700

    added listeners and timer to Engine2D

--- src/com/github/massiv_diversions/engine/Engine2D.java
+++ src/com/github/massiv_diversions/engine/Engine2D.java
@@ -1,5 +1,8 @@
 package com.github.massiv_diversions.engine;
 
+import java.awt.*;
+import java.awt.event.*;
+
 import javax.swing.*;
 
 public class Engine2D {
@@ -7,28 +10,63 @@ public class Engine2D {
 	private ControlData cd;
 	private EngineDetails ed;
 	private JFrame frame;
+	private JPanel panel;
 	private StateManager sm;
 	private Timer timer;
-	
+
 	public Engine2D(ControlData cd, EngineDetails ed, StateManager sm) {
-		// store args
+		// store arguments
 		this.cd = cd;
 		this.ed = ed;
 		this.sm = sm;
-		
+
 		// setup frame
 		frame = new JFrame();
 		frame.setSize(ed.width(), ed.height());
 		frame.setTitle(ed.title());
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        // setup panel
-        
-        // setup timer
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.addKeyListener(new ControlListener());
+
+		// setup panel
+
+		// setup timer
+		timer = new Timer(ed.delay(), new FrameListener());
+		timer.setCoalesce(true);
 	}
-	
+
 	public void start() {
 		frame.setVisible(true);
 		timer.start();
 	}
+
+	public void frame() {
+		sm.update(cd);
+		sm.render((Graphics2D) panel.getGraphics());
+		sm.next();
+	}
+
+	private class ControlListener extends KeyAdapter {
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			cd.press(e.getKeyCode());
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			cd.release(e.getKeyCode());
+		}
+
+	}
+
+	private class FrameListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			frame();
+		}
+
+	}
+
+
 }
