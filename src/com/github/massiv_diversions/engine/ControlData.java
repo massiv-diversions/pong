0e6da5b5738984f68a6137a0f2cc305dc2dd8fdc
Cooper Johnson <lavosprime@gmail.com>
2013-06-17 20:25:34 -0700

    created ControlData

---
+++ src/com/github/massiv_diversions/engine/ControlData.java
@@ -0,0 +1,30 @@
+package com.github.massiv_diversions.engine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControlData {
+
+	private Map<Integer, Boolean> keyStates;
+
+	public ControlData(int... keyValues) {
+		keyStates = new HashMap<Integer, Boolean>();
+		for (int i : keyValues)
+			keyStates.put(i, false);
+	}
+
+	public boolean keyDown(int keyValue) {
+		return keyStates.containsKey(keyValue) && keyStates.get(keyValue);
+	}
+
+	protected void press(int keyValue) {
+		if (keyStates.containsKey(keyValue))
+			keyStates.put(keyValue, true);
+	}
+
+	protected void release(int keyValue) {
+		if (keyStates.containsKey(keyValue))
+			keyStates.put(keyValue, false);
+	}
+
+}
