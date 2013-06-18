acac774e4013c90576a5aee7ed1c4561d9f6a3f6
Cooper Johnson <lavosprime@gmail.com>
2013-06-17 19:50:50 -0700

    added frame and its setup, timer, and start() to Engine2D

--- src/com/github/massiv_diversions/engine/Engine2D.java
+++ src/com/github/massiv_diversions/engine/Engine2D.java
@@ -1,14 +1,34 @@
 package com.github.massiv_diversions.engine;
 
+import javax.swing.*;
+
 public class Engine2D {
 
 	private ControlData cd;
 	private EngineDetails ed;
+	private JFrame frame;
 	private StateManager sm;
+	private Timer timer;
 	
 	public Engine2D(ControlData cd, EngineDetails ed, StateManager sm) {
+		// store args
 		this.cd = cd;
 		this.ed = ed;
 		this.sm = sm;
+		
+		// setup frame
+		frame = new JFrame();
+		frame.setSize(ed.width(), ed.height());
+		frame.setTitle(ed.title());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // setup panel
+        
+        // setup timer
+	}
+	
+	public void start() {
+		frame.setVisible(true);
+		timer.start();
 	}
 }
