b7dbf99d5017f007460b2d2c22f0e1fb9c85b2d3
Cooper Johnson <lavosprime@gmail.com>
2013-06-18 17:26:54 -0700

    adjusted TestState0 for StateManager/GameState refactor

--- src/TestState0.java
+++ src/TestState0.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
+import java.awt.geom.Rectangle2D;
 
 import com.github.massiv_diversions.engine.ControlData;
 import com.github.massiv_diversions.engine.EngineDetails;
@@ -9,9 +10,13 @@ import com.github.massiv_diversions.engine.GameState;
 
 public class TestState0 implements GameState {
 
+	private EngineDetails ed;
 	private int x, y;
+	private Rectangle2D background;
 
 	public TestState0(EngineDetails ed) {
+		this.ed = ed;
+		background = new Rectangle2D.Float(0, 0, ed.width(), ed.height());
 		x = ed.width() / 2;
 		y = ed.height() / 2;
 	}
@@ -22,21 +27,23 @@ public class TestState0 implements GameState {
 		boolean downArrow = cd.keyDown(KeyEvent.VK_DOWN);
 		if (upArrow && !downArrow) {
 			System.out.println("up");
-			y++;
+			y--;
 		} else if (!upArrow && downArrow) {
 			System.out.println("down");
-			y--;
+			y++;
 		}
 	}
 
 	@Override
 	public void render(Graphics2D g) {
-		g.setColor(Color.BLUE);
+		g.setColor(Color.BLACK);
+		g.fill(background);
+		g.setColor(Color.WHITE);
 		g.fillOval(x - 3, y - 3, 7, 7);
 	}
 
 	@Override
-	public GameState next(EngineDetails ed) {
+	public GameState next() {
 		return null;
 	}
 
